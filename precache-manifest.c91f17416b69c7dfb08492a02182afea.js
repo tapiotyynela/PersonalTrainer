f0081de45157cd268b08b873474ef3cfa6fdc178
tapiotyynela <tapio.tyynela@gmail.com>
2019-12-14 09:39:11 +0200

    Updates

---
+++ precache-manifest.c91f17416b69c7dfb08492a02182afea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6945a34be19e04d6a90f07ef6441025",
+    "url": "/PersonalTrainer/index.html"
+  },
+  {
+    "revision": "6b16998fc699aa3b3b95",
+    "url": "/PersonalTrainer/static/css/2.635fce67.chunk.css"
+  },
+  {
+    "revision": "79ddb909a5874cbf1f00",
+    "url": "/PersonalTrainer/static/css/main.a9c84216.chunk.css"
+  },
+  {
+    "revision": "6b16998fc699aa3b3b95",
+    "url": "/PersonalTrainer/static/js/2.da5843be.chunk.js"
+  },
+  {
+    "revision": "79ddb909a5874cbf1f00",
+    "url": "/PersonalTrainer/static/js/main.a7d07fb8.chunk.js"
+  },
+  {
+    "revision": "74b2b93eb9693665a21f",
+    "url": "/PersonalTrainer/static/js/runtime-main.c2313b8a.js"
+  }
+]);
